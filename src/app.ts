8c1c6028017b23cb825aee9c81fe3af22ccb88d3
alaminwbsoft <wbsoft.alamin@gmail.com>
2025-05-06 21:07:13 +0600

    add

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+console.log(process.cwd());
+
+export default app;
